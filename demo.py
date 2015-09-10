73e9ac7b649b253c172c2e9cf39908b19e6a7485
Hans Gerritsen <hans.gerritsen@xs4all.nl>
2015-09-10 08:33:00 +0200

    add new function demo2

--- demo.py
+++ demo.py
@@ -1,7 +1,11 @@
 def demo1(text):
     print text,"dit is demo1"
 
+def demo2(par1, par2):
+    print "som van", par1, "en", par2, "=", par1 + par2
+    
 if __name__ == "__main__":
     print "Start"
     demo1('Hallo')
+    demo2(1,2)
     
