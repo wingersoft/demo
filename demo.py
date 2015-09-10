157f7c1fcaf04b8f21e29227faa9fd767961ac41
Hans Gerritsen <hans.gerritsen@xs4all.nl>
2015-09-10 08:41:42 +0200

    add som en verschil functions

--- demo.py
+++ demo.py
@@ -1,11 +1,15 @@
 def demo1(text):
     print text,"dit is demo1"
 
-def demo2(par1, par2):
+def som(par1, par2):
     print "som van", par1, "en", par2, "=", par1 + par2
+    
+def verschil(par1, par2):
+    print "verschil van", par1, "en", par2, "=", par1 - par2
     
 if __name__ == "__main__":
     print "Start"
     demo1('Hallo')
-    demo2(1,2)
+    som(33, 66)
+    verschil(55, 44)
     
