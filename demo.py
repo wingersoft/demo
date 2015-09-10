372a35e26f9269c3914672e2c66f75cb8559b215
Hans Gerritsen <hans.gerritsen@xs4all.nl>
2015-09-10 09:06:33 +0200

    Fixed function error

--- demo.py
+++ demo.py
@@ -6,10 +6,14 @@ def som(par1, par2):
     
 def verschil(par1, par2):
     print "verschil van", par1, "en", par2, "=", par1 - par2
+
+def error(par1, par2, par3):
+    print par1, par2, par3
     
 if __name__ == "__main__":
     print "Start"
     demo1('Hallo')
     som(33, 66)
     verschil(55, 44)
+    error(1, 2, 3)
     
