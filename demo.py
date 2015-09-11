aae3233a837fa21aafe4f582eab5cb0c21273936
Hans Gerritsen <hans.gerritsen@xs4all.nl>
2015-09-11 08:29:49 +0200

    add function add()

--- demo.py
+++ demo.py
@@ -1,7 +1,11 @@
 def demo1(text):
     print text,"dit is een demo"
 
+def add(par1, par2):
+    print par1, "+", par2, "=", par1 + par2
+
 if __name__ == "__main__":
     print "Start"
     demo1('Hallo')
+    add(22, 55)
     
